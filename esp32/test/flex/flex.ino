09884bc9bebe019492e0edd7c717a28cb94e834c
NIHAL  T P <nihaltpnki@gmail.com>
2026-09-05 18:28:03 +0530

    move flex sensor code to test dir

--- esp32/test/flex/flex.ino
+++ esp32/test/flex/flex.ino
@@ -1,12 +1,11 @@
-
-const int flexPin = 34; 
+const int flexPin = 34;
 
 void setup() {
   // Start the Serial communication at 115200 baud rate
   Serial.begin(115200);
-  
+
   // Wait a moment for the serial port to initialize
-  delay(1000); 
+  delay(1000);
   Serial.println("Flex Sensor Test Started.");
 }
 
@@ -18,6 +17,5 @@ void loop() {
   Serial.print("Raw Flex Value: ");
   Serial.println(flexValue);
 
-
-  delay(100); 
+  delay(100);
 }
