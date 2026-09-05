753d7bc86b8478a453199101776f699a12ea4370
NIHAL  T P <nihaltpnki@gmail.com>
2026-09-06 02:07:10 +0530

    feat: add LED control functionality with blink support

---
+++ esp32/main/LED.h
@@ -0,0 +1,23 @@
+#ifndef LED_H
+#define LED_H
+
+#include "PIN.h"
+
+#include <Arduino.h>
+#include <Ticker.h>
+
+class LED {
+public:
+  static void begin();
+  static void on();
+  static void off();
+  static void setState(bool state);
+  static void blink(uint32_t interval_ms);
+  static void stopBlink();
+  static void toggle();
+private:
+  static Ticker blinker;
+  static bool state;
+};
+
+#endif
