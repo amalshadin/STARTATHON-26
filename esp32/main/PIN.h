7eaa2ece92c0ba5ea0ff31c456071b489005d64b
NIHAL  T P <nihaltpnki@gmail.com>
2026-09-06 02:07:10 +0530

    feat: add LED control functionality with blink support

--- esp32/main/PIN.h
+++ esp32/main/PIN.h
@@ -1,5 +1,5 @@
-#ifndef PIN_H
-#define PIN_H
+#ifndef MY_PROJECT_PIN_H
+#define MY_PROJECT_PIN_H
 
 struct PIN {
   static const int flexPin1 = 34;
@@ -7,6 +7,7 @@ struct PIN {
   static const int flexPin3 = 36;
   static const int SDAPin = 21;
   static const int SCLPin = 22;
+  static const int ledPin = 2; // Built-in LED pin on ESP32
 };
 
 #endif
