acf9ba52e1092c36a921492bac5824e78c658e4e
NIHAL  T P <nihaltpnki@gmail.com>
2026-09-06 09:17:55 +0530

    feat: update BLE device name initialization and add setter method

--- esp32/main/BLE.cpp
+++ esp32/main/BLE.cpp
@@ -1,7 +1,5 @@
 #include "BLE.h"
 
-String BLEManager::deviceName = "ESP32-CS-ROVER";
-
 #include <BLEDevice.h>
 #include <BLEUtils.h>
 #include <BLEServer.h>
@@ -10,6 +8,8 @@ String BLEManager::deviceName = "ESP32-CS-ROVER";
 
 #define SERVICE_UUID "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
 #define CHARACTERISTIC_UUID "beb5483e-36e1-4688-b7f5-ea07361b26a8"
+
+String BLEManager::deviceName = "ESP32-CS-ROVER-TEST";
 
 class MyCallbacks : public BLECharacteristicCallbacks {
   void onWrite(BLECharacteristic *pCharacteristic) {
