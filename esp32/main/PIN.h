b305cb38ea4836d1af10f78376a93ab0990dca2e
NIHAL  T P <nihaltpnki@gmail.com>
2026-09-12 21:55:50 +0530

    fix: correct flexPin assignments in PIN.h

--- esp32/main/PIN.h
+++ esp32/main/PIN.h
@@ -2,9 +2,9 @@
 #define MY_PROJECT_PIN_H
 
 struct PIN {
-  static const int flexPin1 = 34;
-  static const int flexPin2 = 35;
-  static const int flexPin3 = 36;
+  static const int flexPin1 = 14;
+  static const int flexPin2 = 26;
+  static const int flexPin3 = 33;
   static const int SDAPin = 21;
   static const int SCLPin = 22;
   static const int ledPin = 2; // Built-in LED pin on ESP32
